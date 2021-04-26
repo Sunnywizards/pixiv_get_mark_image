a3a69be08c6cdbf429d50cf7701e33b7dd850f64
sunnywizards <sunnywizards@163.com>
2021-04-26 23:24:31 +0800

    improve security

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 
+import getpass
+
 from process.process import *
 
 if __name__ == "__main__":
     account = input("Input your pixiv account\n")
-    pw = input("Input your password\n")
+    pw = getpass.getpass("Input your password\n")
     browser = webdriver.Chrome(CHROME_DRIVER_ADDRESS)
     login_pixiv(browser, account, pw)
     get_pixiv_mark_image(browser)
